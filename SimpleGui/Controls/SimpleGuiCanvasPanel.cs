efd735e27febe1d250873c611597bbeb5e273873
Franz Alex Gaisie-Essilfie <falexge@gmail.com>
2015-07-06 07:20:05 +0000

    Optimization of canvas
     * Double buffered to reduce flicker.
     * Added properties for interacting with the frame rate.
     * Mouse events are triggered even when not clicking on canvas.
     * Optimized starting and stopping of canvas drawing.

--- SimpleGui/Controls/SimpleGuiCanvasPanel.cs
+++ SimpleGui/Controls/SimpleGuiCanvasPanel.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using SimpleGui.Drawing;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
-using Canvas = SimpleGui.Drawing.Canvas;
+using BindingFlags = System.Reflection.BindingFlags;
 
 namespace SimpleGui.Controls
 {
@@ -13,30 +14,46 @@ namespace SimpleGui.Controls
         private KeyEventHandler _keyUpHandler;
         private MouseEventHandler _mouseClickHandler;
         private MouseEventHandler _mouseDragHandler;
+        private int _targetFps;
         private Canvas canvas;
-        private SimpleGuiForm Form;
-        private long lastFrame;
+        private bool formActive;
+        private FpsCounter fpsCounter;
+        private long lastFrameTime;
         private bool stopRendering;
+        private SimpleGuiForm thisForm;
 
         public SimpleGuiCanvasPanel(SplitterPanel splitterPanel)
             : base(splitterPanel)
         {
-            this.Form = this.Parent.ParentForm as SimpleGuiForm;
-            this.Form.KeyPreview = true;
-            this.Form.KeyDown += (o, e) => { if (_keyDownHandler != null) _keyDownHandler(e.KeyCode); };
-            this.Form.KeyUp += (o, e) => { if (_keyUpHandler != null) _keyUpHandler(e.KeyCode); };
+            // set thisForm's properties and event handlers
+            thisForm = this.Parent.ParentForm as SimpleGuiForm;
+            thisForm.KeyPreview = true;
 
+            thisForm.KeyDown += (o, e) => { if (_keyDownHandler != null) _keyDownHandler(e.KeyCode); };
+            thisForm.KeyUp += (o, e) => { if (_keyUpHandler != null) _keyUpHandler(e.KeyCode); };
+            thisForm.Deactivate += thisForm_Deactivate;
+            thisForm.Activated += thisForm_Activated;
+            thisForm.FormClosing += (o, e) => this.Stop();
+
+
+            // set panel properties and event handlers
             Panel.BackColor = Color.White;
+            Panel.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic)
+                           .SetValue(Panel, true, null);    // make panel double buffered to remove flicker
 
             Panel.MouseClick += splitterPanel_MouseClick;
             Panel.MouseMove += splitterPanel_MouseMove;
             Panel.Paint += Panel_Paint;
 
+
+            // set the canvas and frame rate
             canvas = new Canvas() {
                 BackgroundColor = Color.Black,
                 Owner = splitterPanel,
                 Size = new Size(480, 360),
             };
+
+            this.TargetFrameRate = 30;
         }
 
         /// <summary>Gets or sets the background color of the canvas.</summary>
@@ -53,9 +70,28 @@ namespace SimpleGui.Controls
             set { canvas.Size = value; }
         }
 
+        /// <summary>Gets the current frame rate.</summary>
+        public double FrameRate { get { return fpsCounter.FPS; } }
+
         /// <summary>Gets a value indicating whether the canvas is running.</summary>
         /// <value><c>true</c> if the canvas is running; otherwise, <c>false</c>.</value>
         public bool IsRunning { get { return _isRunning; } }
+
+        /// <summary>Gets or sets the target frame rate at which the canvas is to be updated.</summary>
+        public int TargetFrameRate
+        {
+            get { return _targetFps; }
+            set
+            {
+                _targetFps = value;
+                fpsCounter = new FpsCounter(value * 2);
+                if (_isRunning)
+                {
+                    this.Stop();
+                    this.Start();
+                }
+            }
+        }
 
         /// <summary>Gets or sets a value indicating whether the panel is visible.</summary>
         /// <value><c>true</c> if the panel is visible; otherwise, <c>false</c>.</value>
@@ -63,15 +99,24 @@ namespace SimpleGui.Controls
         {
             get
             {
-                return base.Visible;
+                return !thisForm.spltControls.Panel2Collapsed && base.Visible;
             }
             set
             {
                 base.Visible = value;
 
-                if (value && Form.spltControls.Panel2Collapsed)
-                    Form.spltControls.Panel2Collapsed = false;
+                if (value && thisForm.spltControls.Panel2Collapsed)
+                    thisForm.spltControls.Panel2Collapsed = false;
             }
+        }
+
+        /// <summary>Gets a screen-shot of the canvas.</summary>
+        public Image GetScreenshot()
+        {
+            if (_drawHandler != null)
+                return canvas.CreateScreenshot(_drawHandler);
+            else
+                return null;
         }
 
         /// <summary>Sets an event handler for drawing frames to the canvas.</summary>
@@ -118,10 +163,10 @@ namespace SimpleGui.Controls
 
         /// <summary>Starts drawing the canvas at the specified FPS.</summary>
         /// <param name="fps">The number of frames to be drawn per second.</param>
-        public void Start(int fps = 30)
+        internal void Start()
         {
-            var fpsDelta = 1000 / fps;                  // time delta between two frames at specified FPS
-            var sleepTime = Math.Max(fpsDelta / 10, 1); // the time to sleep while waiting for next frame; 1/10 of frame rate
+            var fpsDelta = 1000 / _targetFps;  // time delta between two frames at specified FPS
+            var sleepTime = 1;                 // the time to sleep while waiting for next frame
 
             stopRendering = false;  // make sure the canvas isn't in paused state
             _isRunning = true;      // frames are being rendered
@@ -129,25 +174,35 @@ namespace SimpleGui.Controls
             do
             {
                 var frameTime = Environment.TickCount;
-                if (frameTime >= lastFrame + fpsDelta)
+                if (frameTime >= lastFrameTime + fpsDelta)
                 {
-                    lastFrame = frameTime;
-                    Form.spltCanvas.Panel1.Invalidate();
+                    lastFrameTime = frameTime;
+                    Panel.Invalidate(canvas.Rectangle);
                     Application.DoEvents();
                 }
                 else
                 {
                     System.Threading.Thread.Sleep(sleepTime);
                 }
-            } while (!stopRendering);
+            } while (CanRenderNextFrame());
 
             _isRunning = false;
+            stopRendering = false;
         }
 
         /// <summary>Stops rendering the canvas.</summary>
-        public void Stop()
+        internal void Stop()
         {
             stopRendering = true;
+            fpsCounter.SkipNextFrame();
+        }
+
+        /// <summary>Determines whether this instance can render the next frame.</summary>
+        /// <returns><c>true</c> if the next frame can be rendered; otherwise <c>false</c>.</returns>
+        private bool CanRenderNextFrame()
+        {
+            return thisForm.Visible && formActive &&
+                   !stopRendering && this.Visible;
         }
 
         /// <summary>Handles the Paint event of the Panel control.</summary>
@@ -164,10 +219,18 @@ namespace SimpleGui.Controls
                 e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
                 e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
 
-                canvas.Graphics = e.Graphics;
-                _drawHandler.Invoke(canvas);
-                canvas.Graphics = null;
+                try
+                {
+                    canvas.Graphics = e.Graphics;
+                    _drawHandler.Invoke(canvas);
+                }
+                finally
+                {
+                    canvas.Graphics = null;
+                }
             }
+
+            fpsCounter.MarkFrame();
         }
 
         /// <summary>Handles the MouseClick event of the splitterPanel control.</summary>
@@ -187,12 +250,36 @@ namespace SimpleGui.Controls
         /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
         private void splitterPanel_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Button != MouseButtons.None && _mouseDragHandler != null &&
-                canvas.Rectangle.Contains(e.Location))
+            if (e.Button != MouseButtons.None && _mouseDragHandler != null)
             {
                 Point pos = canvas.ClientToCanvas(e.Location).ToPoint();
                 _mouseDragHandler(pos, e.Button);
             }
+        }
+
+        private void StartStop_Canvas(object sender, EventArgs e)
+        {
+            if (CanRenderNextFrame())
+            {
+                if (!_isRunning)
+                    this.Start();
+            }
+            else
+            {
+                this.Stop();
+            }
+        }
+
+        private void thisForm_Activated(object sender, EventArgs e)
+        {
+            formActive = true;
+            StartStop_Canvas(sender, e);
+        }
+
+        private void thisForm_Deactivate(object sender, EventArgs e)
+        {
+            formActive = false;
+            StartStop_Canvas(sender, e);
         }
     }
 }
