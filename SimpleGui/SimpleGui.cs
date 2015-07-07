e0ebf753411a06ad4c153cbc2286f2817ca25c4e
Franz Alex Gaisie-Essilfie <falexge@gmail.com>
2015-07-07 02:23:09 +0000

    Fixed code to dispose timers when Form closes.

--- SimpleGui/SimpleGui.cs
+++ SimpleGui/SimpleGui.cs
@@ -1,5 +1,6 @@
 ﻿using SimpleGui.Controls;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using ProjectResources = SimpleGui.Properties.Resources;
 
@@ -7,18 +8,22 @@ namespace SimpleGui
 {
     public class SimpleGui
     {
-        private SimpleGuiForm Form;
-        private SimpleGuiControlsPanel _controlsPanel;
         private SimpleGuiCanvasPanel _canvasPanel;
+        private SimpleGuiControlsPanel _controlsPanel;
         private SimpleGuiLogPanel _logPanel;
+        private SimpleGuiForm Form;
+        private List<Timer> timers;
 
         /// <summary>Creates a new instance of the <see cref="SimpleGui"/> class.</summary>
         public SimpleGui()
         {
+            timers = new List<Timer>();
+
             Form = new SimpleGuiForm() {
                 Text = "Simple GUI",
                 Icon = ProjectResources.AppIcon
             };
+            Form.FormClosed += Form_FormClosed;
 
             _controlsPanel = new SimpleGuiControlsPanel(Form.spltControls.Panel1);
             _canvasPanel = new SimpleGuiCanvasPanel(Form.spltCanvas.Panel1);
@@ -60,20 +65,6 @@ namespace SimpleGui
             return this.CreateTimer(interval, (tmr) => tickHandler.Invoke());
         }
 
-        /// <summary>Creates timer associated with the form.</summary>
-        /// <param name="interval">
-        /// The interval in milliseconds between consecutive ticks of the timer.
-        /// </param>
-        /// <param name="tickHandler">The event handler for the timer's tick.</param>
-        /// <returns></returns>
-        private SimpleTimer CreateTimer(int interval, MultiControlEventHandler tickHandler)
-        {
-            var tmr = new Timer(Form.Components) { Interval = interval };
-            var sTmr = new SimpleTimer(tmr);
-            tmr.Tick += (o, e) => tickHandler(sTmr);
-            return sTmr;
-        }
-
         /// <summary>Starts running the SimpleGui program.</summary>
         public void Start()
         {
@@ -86,6 +77,36 @@ namespace SimpleGui
             {
                 if (ex.GetType() != typeof(ObjectDisposedException))
                     MessageBox.Show("Could not run the SimpleGui application.");
+            }
+        }
+
+        /// <summary>Creates timer associated with the form.</summary>
+        /// <param name="interval">
+        /// The interval in milliseconds between consecutive ticks of the timer.
+        /// </param>
+        /// <param name="tickHandler">The event handler for the timer's tick.</param>
+        /// <returns></returns>
+        private SimpleTimer CreateTimer(int interval, MultiControlEventHandler tickHandler)
+        {
+            var tmr = new Timer() { Interval = interval };
+            var sTmr = new SimpleTimer(tmr);
+
+            timers.Add(tmr);
+            tmr.Tick += (o, e) => tickHandler(sTmr);
+
+            return sTmr;
+        }
+
+        /// <summary>Handles the FormClosed event of the Form control.</summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="FormClosedEventArgs"/> instance containing the event data.</param>
+        private void Form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // stop and dispose all timers that have been created
+            foreach (var tmr in timers)
+            {
+                tmr.Stop();
+                tmr.Dispose();
             }
         }
     }
